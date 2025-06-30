906c8f9d733176f37a8223a6b6fff07f1f936753
k-redstone <che0618@naver.com>
2025-06-30 22:38:28 +0900

    feat: Footer 일부 수정

--- src/components/common/Footer.tsx
+++ src/components/common/Footer.tsx
@@ -1,10 +1,34 @@
+import Link from 'next/link'
+
 export default function Footer() {
   return (
     <footer className="bg-card border-border border-t px-4 py-6">
       <div className="mx-auto max-w-6xl text-center">
-        <p className="text-muted-foreground text-sm">
-          핑맨과 함께하는 레인보우 식스 시즈 X: 시즈킹
-        </p>
+        <div className="flex justify-between">
+          <div className="flex items-center justify-center">
+            <p className="text-muted-foreground text-sm">
+              틀린 정보 제보: che06189@gmail.com
+            </p>
+          </div>
+          <div className="text-muted-foreground flex flex-col gap-y-1 text-sm">
+            <p>Family Site</p>
+
+            <Link
+              prefetch={false}
+              className="text-end"
+              href={'https://saebaldong.kr/'}
+            >
+              - 새발동
+            </Link>
+            <Link
+              prefetch={false}
+              className="text-end"
+              href={'https://lol.saebaldong.kr/'}
+            >
+              - 낙오전
+            </Link>
+          </div>
+        </div>
       </div>
     </footer>
   )
