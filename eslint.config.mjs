1437713f6b0d37b0d34fe2068797ddb21a12577e
k-redstone <che0618@naver.com>
2025-06-30 17:06:24 +0900

    chore: 프로젝트 기본 세팅

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,16 +1,85 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
+import { dirname } from 'path'
+import { fileURLToPath } from 'url'
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+import { FlatCompat } from '@eslint/eslintrc'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
 
 const compat = new FlatCompat({
   baseDirectory: __dirname,
-});
+})
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
-];
+  ...compat.extends('next/core-web-vitals', 'next/typescript'),
+  {
+    ignores: ['node_modules', '.next', 'out', 'public', 'dist', 'build'],
+    rules: {
+      'import/order': [
+        'error',
+        {
+          groups: [
+            'builtin',
+            'external',
+            'internal',
+            ['parent', 'sibling'],
+            'index',
+            'object',
+            'type',
+            'unknown',
+          ],
 
-export default eslintConfig;
+          pathGroups: [
+            {
+              pattern: 'next',
+              group: 'builtin',
+            },
+            {
+              pattern: 'next/',
+              group: 'builtin',
+            },
+            {
+              pattern: 'react',
+              group: 'builtin',
+              position: 'before',
+            },
+            {
+              pattern: '@tanstack/',
+              group: 'external',
+            },
+            {
+              pattern: '@/components/**',
+              group: 'internal',
+            },
+            {
+              pattern: '@/libs/**',
+              group: 'unknown',
+            },
+            {
+              pattern: '@/core/**',
+              group: 'unknown',
+            },
+            {
+              pattern: '@/store/**',
+              group: 'unknown',
+            },
+            {
+              pattern: '**/*.css.ts',
+              group: 'unknown',
+              position: 'after',
+            },
+          ],
+
+          'newlines-between': 'always',
+
+          alphabetize: {
+            order: 'asc',
+            caseInsensitive: true,
+          },
+        },
+      ],
+    },
+  },
+]
+
+export default eslintConfig
---
+++ src/components/common/XIcon.tsx
@@ -0,0 +1,19 @@
+export default function XIcon() {
+  return (
+    <div className="inline-flex h-10 w-10 items-center justify-center rounded-lg bg-black shadow">
+      <svg
+        className="h-8 w-8"
+        viewBox="0 0 24 24"
+        fill="none"
+        stroke="none"
+      >
+        <path
+          d="M6 6L18 18M6 18L18 6"
+          stroke="#FF3D3D"
+          strokeWidth="4"
+          strokeLinecap="round"
+        />
+      </svg>
+    </div>
+  )
+}
