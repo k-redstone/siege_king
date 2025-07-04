b382b497101a28a83cdb29b06f3e8f87acf6248d
k-redstone <che0618@naver.com>
2025-07-04 19:49:05 +0900

    fix: useSearchParams를 사용하는 page를 클라이언트화하여 빌드에러 해결

--- src/app/match-records/page.tsx
+++ src/app/match-records/page.tsx
@@ -1,69 +1,15 @@
 'use client'
 
-import DetailMatchInfo from '@/components/match/DetailMatchInfo'
-import { Button } from '@/components/ui/button'
-import { DETAIL_MATCH_INFO } from '@/constants/matchDetail'
+import { Suspense } from 'react'
+
+import MatchReocrdsClient from '@/components/match/MatchReocrdsClient'
 import { GAPageView } from '@/hooks/useGAPageViesw'
-import { useTabQuery, MATCH_RECORDS_TABS } from '@/hooks/useTabQuery'
 
 export default function MatchRecordsPage() {
-  const { tab, setTab } = useTabQuery()
-
-  const filteredMatches =
-    tab === 0
-      ? DETAIL_MATCH_INFO
-      : DETAIL_MATCH_INFO.filter((match) =>
-          tab === 1 ? !match.isScrim : match.isScrim,
-        )
-
   return (
-    <div className="bg-background text-foreground py-10">
-      <div className="mx-auto max-w-6xl px-4">
-        {/* Header */}
-        <div className="mb-12 text-center">
-          <h1 className="mb-4 text-3xl font-bold md:text-4xl">매치 기록</h1>
-          {/* <p className="text-muted-foreground text-lg">경기 결과 및 통계</p> */}
-          <p className="text-muted-foreground text-lg">
-            공식 스크림 및 토너먼트 결과만 반영합니다.
-          </p>
-          <div className="mt-4"></div>
-          <p className="text-muted-foreground text-base">
-            매치 기록은 아직 작업이 안 끝났습니다. 공식 스크림이 진행되기 전까지
-            업데이트 될 예정입니다!
-          </p>
-          <p className="text-muted-foreground text-base">
-            매치 상세 기록은 세트별로 묶어서 제공 될 예정입니다.
-          </p>
-        </div>
-
-        {/* Filter Tabs */}
-        <div className="mb-6 flex justify-center gap-2">
-          {MATCH_RECORDS_TABS.map((matchTab) => (
-            <Button
-              className="cursor-pointer"
-              key={matchTab.label}
-              variant={tab === matchTab.id ? 'default' : 'outline'}
-              onClick={() => setTab(matchTab.id)}
-            >
-              {matchTab.label}
-            </Button>
-          ))}
-        </div>
-
-        {/* Match Results */}
-        <section className="mb-12">
-          <h2 className="mb-6 text-2xl font-bold">경기 결과</h2>
-          <div className="space-y-4">
-            {filteredMatches.map((match) => (
-              <DetailMatchInfo
-                key={match.id}
-                match={match}
-              />
-            ))}
-          </div>
-        </section>
-      </div>
+    <Suspense>
+      <MatchReocrdsClient />
       <GAPageView />
-    </div>
+    </Suspense>
   )
 }
